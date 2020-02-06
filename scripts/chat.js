679a7131f6027fa2d0c6855095f05722441475ec
lloydblake <lloyd.blake@footballndex.co.uk>
2020-02-06 13:52:48 +0000

    added firebase config and updated chat.js

--- scripts/chat.js
+++ scripts/chat.js
@@ -2,7 +2,7 @@ class Chatroom {
   constructor(room, username) {
     this.room = room;
     this.username = username;
-    this.db = db.collection("chats");
+    this.chats = db.collection("chats");
   }
   async addChat(message) {
     const now = new Date();
@@ -15,4 +15,33 @@ class Chatroom {
     const response = await this.chats.add(chat);
     return response;
   }
+  getChats(callback) {
+    this.chats
+      .where("room", "==", this.room)
+      .orderBy("created_at")
+      .onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type === "added") {
+            callback(change.doc.data());
+          }
+        });
+      });
+  }
+  updateName(username) {
+    this.username = username;
+  }
+  updateRoom(room) {
+    this.room = room;
+  }
 }
+
+const chatroom = new Chatroom("gaming", "Lloyd");
+
+chatroom
+  .addChat("hello everyone")
+  .then(() => console.log("chat added"))
+  .catch(err => console.log(err));
+
+chatroom.getChats(data => {
+  console.log(data);
+});
